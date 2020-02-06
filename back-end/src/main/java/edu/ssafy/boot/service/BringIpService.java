ef73474f9a96d850fc9b56aab4b639d11e4b9795
최성용 <queer1234@naver.com>
2020-02-06 17:57:32 +0900

    no message

--- back-end/src/main/java/edu/ssafy/boot/service/BringIpService.java
+++
@@ -1,5 +0,0 @@
-package edu.ssafy.boot.service;
-
-public class BringIpService {
-
-}
--- back-end/src/main/java/edu/ssafy/boot/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package edu.ssafy.boot.service;
-
-import javax.mail.MessagingException;
-
-public interface EmailService {
-	public void findPassword(String email) throws MessagingException;
-	public String checkEmail(String email) throws MessagingException;
-}
